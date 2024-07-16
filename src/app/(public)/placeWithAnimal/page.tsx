09e8b7a1a213f488d44a18871c8c069f2fbb23c9
hyeonw8 <whgusrud223@gmail.com>
2024-07-17 02:04:09 +0900

    add: 반려동물 동반 가능 페이지
    데이터 받아오는 부분 쿼리 이용해서 리팩터링 필요

---
+++ src/app/(public)/placeWithAnimal/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect, useState } from "react"
+import PlaceItem from "./_components/PlaceItem";
+import { PlaceData } from "@/types/place";
+
+
+const PlaceWithAnimal = () => {
+  const [placeData, setPlaceData] = useState<PlaceData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/withPetApi');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setPlaceData(data.data);
+      } catch (error) {
+        console.error("장소 데이터를 가져오는 데 실패했습니다:", error);
+      }
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(placeData);
+
+  return (
+    <div>
+      {
+        placeData.map((place) => <PlaceItem place={place} />)
+      }
+    </div>
+  )
+}
+
+export default PlaceWithAnimal
