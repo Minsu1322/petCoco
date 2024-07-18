377453c807a0eff2d087347069901bba51ba942a
hyeonw8 <whgusrud223@gmail.com>
2024-07-18 09:25:44 +0900

    add: post GET, POST 라우트 핸들러 추가
    POST는 이미지 업로드 기능 추가해야 함(전체적으로 react-quill 적용해 보기)

---
+++ src/app/api/mate/route.ts
@@ -0,0 +1,41 @@
+import { createClient } from "@/supabase/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const supabase = createClient();
+
+  try {
+    const { data, error } = await supabase.from("matePosts").select("*").order("created_at", { ascending: false });
+    // .limit(10);
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ err }, { status: 500 });
+  }
+};
+
+export const POST = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const supabase = createClient();
+  //const { id } = params;
+  const { title, content } = await request.json();
+
+  try {
+    const { data, error } = await supabase.from("matePosts").insert({ title, content });
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ err }, { status: 500 });
+  }
+};
