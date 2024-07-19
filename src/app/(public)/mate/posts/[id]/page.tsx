a218a3d32b63a9807b6a2cd7cccdc0651990e4a3
hyeonw8 <whgusrud223@gmail.com>
2024-07-19 16:04:25 +0900

    add: 산책 메이트 글 상세 페이지 생성

---
+++ src/app/(public)/mate/posts/[id]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import DetailMatePost from "../_components/DetailMatePost";
+
+const MatePost = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+
+  const {
+    data: post,
+    isPending,
+    error
+  } = useQuery({
+    queryKey: ["matePosts", id],
+    queryFn: async () => {
+      const response = await fetch(`/api/mate/post/${id}`);
+      const data = response.json();
+      console.log(data);
+
+      return data;
+    }
+  });
+
+  if(!post) return;
+
+  if (isPending) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  console.log(post);
+
+  return (<div>
+    <DetailMatePost post={post} />
+  </div>);
+};
+
+export default MatePost;
