7f0843023c2b30d99930f4b243fc22c17f7b64b1
hyeonw8 <whgusrud223@gmail.com>
2024-07-21 22:50:42 +0900

    update: MatePostFullType 타입 정의 추가

--- src/types/mate.type.ts
+++ src/types/mate.type.ts
@@ -1,5 +1,16 @@
 import { Tables } from "./supabase";
 export type MatePostType = Tables<"matePosts">;
 
+export type MatePostFullType = MatePostType & {
+  position: {
+    center: {
+      lat: number;
+      lng: number;
+    };
+    errMsg: string | null;
+    isLoading: boolean;
+  }
+}
+
 export type MateCommentType = Tables<"mateComments">;
 
