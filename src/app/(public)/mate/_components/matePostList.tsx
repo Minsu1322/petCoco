7c304069765e1818bade48e20b4b6332a4487b61
hyeonw8 <whgusrud223@gmail.com>
2024-07-22 01:07:08 +0900

    fix: 타입 변경 및 css 수정

--- src/app/(public)/mate/_components/matePostList.tsx
+++ src/app/(public)/mate/_components/matePostList.tsx
@@ -1,15 +1,15 @@
-import { MatePostType } from "@/types/mate.type"
+import { MatePostFullType } from "@/types/mate.type"
 import MatePostItem from './matePostItem';
 
 
 
 interface MatePostListProps {
-  posts: MatePostType[]
+  posts: MatePostFullType[]
 }
 
 const MatePostList = ({posts}: MatePostListProps) => {
   return (
-    <div>
+    <div className="grid grid-cols-2 justify-items-center mt-5">
       {posts.map((post) => <MatePostItem key={post.id} post={post} />)}
     </div>
   )
