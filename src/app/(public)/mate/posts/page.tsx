6a3441fc8b623aafcbc4b9038de02177cff7abdf
hyeonw8 <whgusrud223@gmail.com>
2024-07-22 01:06:42 +0900

    update: 뒤로가기 버튼 추가

--- src/app/(public)/mate/posts/page.tsx
+++ src/app/(public)/mate/posts/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import PostForm from "../_components/postForm";
-
 
 const MatePost = ({ params }: { params: { id: string } }) => {
   return (
-    <div>
+    <div className="px-2">
+      <Link href='/mate'>
+        <div className="mt-3 flex h-10 w-20 items-center justify-center rounded-md bg-mainColor p-1 cursor-pointer">뒤로가기</div>
+      </Link>
       <PostForm />
     </div>
   );
