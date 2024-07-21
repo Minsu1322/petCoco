01e91b31190edda3cd23f5bf82d20bd45d478e2a
hyeonw8 <whgusrud223@gmail.com>
2024-07-22 01:04:18 +0900

    fix: 내부 텍스트가 길어질 경우 줄임 처리를 위해 text 부분을 따로 추가하여 CSS 부분을 따로 설정할 수 있도록 변경

--- src/app/(public)/mate/_components/itemButton.tsx
+++ src/app/(public)/mate/_components/itemButton.tsx
@@ -1,11 +1,14 @@
 interface ItemButtonProps {
   text: string | null;
   className?: string;
+  p_className?: string;
 }
 
-const ItemButton = ({text, className}: ItemButtonProps) => {
+const ItemButton = ({text, className, p_className}: ItemButtonProps) => {
   return (
-    <div className={className}>{text}</div>
+    <div className={className}>
+      <p className={p_className}>{text}</p>
+    </div>
   )
 }
 
