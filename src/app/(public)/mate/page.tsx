e2f26b73f8c8a570eff61e2742bf79624eccccf6
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 02:00:21 +0900

    update: 모집 완료된 메이트도 보기 필터 기능 추가

--- src/app/(public)/mate/page.tsx
+++ src/app/(public)/mate/page.tsx
@@ -4,14 +4,18 @@ import { useQuery } from "@tanstack/react-query";
 
 import Link from "next/link";
 import MatePostList from "./_components/matePostList";
+import SearchBar from "./_components/searchBar";
+import { useState } from "react";
+
+import { MatePostFullType } from "@/types/mate.type";
 
 const MatePage = () => {
   // 커스텀 훅으로 빼기
   const {
-    data: posts,
+    data: allPosts,
     isPending,
     error
-  } = useQuery({
+  } = useQuery<MatePostFullType[]>({
     queryKey: ["matePosts"],
     queryFn: async () => {
       const response = await fetch(`/api/mate`);
@@ -20,6 +24,17 @@ const MatePage = () => {
       return data;
     }
   });
+
+  const [isCurrentPosts, setIstCurrentPosts] = useState<boolean>(true);
+  const currentPosts = allPosts?.filter(post => post.recruiting === true) || [];
+
+  const handleDateSort = () => {
+    //
+  };
+
+  const handleToggleAllPosts = () => {
+    setIstCurrentPosts(!isCurrentPosts);
+  };
 
   if (isPending) {
     return <div>로딩 중,,,</div>;
@@ -31,14 +46,25 @@ const MatePage = () => {
   //console.log(posts);
 
   return (
-    <div>
-      <div>
-        <p className="mb-5">산책 메이트</p>
-        <div className="mb-4 h-10 w-[180px] rounded-lg bg-mainColor p-2 text-center">
-          <Link href="/mate/posts">산책 메이트 구하기 🐾</Link>
+    <div className="mx-8">
+      <h1 className="mb-5 text-center text-2xl">산책 메이트</h1>
+      <div className="mx-12">
+        <SearchBar />
+        <div className="flex flex-row justify-between">
+          <Link href="/mate/posts" className="mb-4 h-10 w-[180px] rounded-lg bg-mainColor p-2 text-center">
+            <div>글쓰기 🐾</div>
+          </Link>
         </div>
-        <MatePostList posts={posts} />
+        <div className="mb-3 flex flex-row justify-between">
+          <div className="cursor-pointer" onClick={handleToggleAllPosts}>{isCurrentPosts ? '모집 완료된 메이트도 보기' : '모집 중인 메이트만 보기'}</div>
+          <div className="flex flex-row gap-x-2">
+            <div onClick={handleDateSort}>마감 임박순</div>
+            <p>|</p>
+            <div>가까운 순</div>
+          </div>
+        </div>
       </div>
+      <MatePostList posts={isCurrentPosts ? currentPosts : allPosts} />
     </div>
   );
 };
