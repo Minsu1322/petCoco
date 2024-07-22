4a2de147ec697e647c4ef6046dffe8dc5fe7c595
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 02:01:52 +0900

    fix: 뒤로가기 버튼 이동

--- src/app/(public)/mate/posts/page.tsx
+++ src/app/(public)/mate/posts/page.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
+
 import PostForm from "../_components/postForm";
 
 const MatePost = ({ params }: { params: { id: string } }) => {
   return (
     <div className="px-2">
-      <Link href='/mate'>
-        <div className="mt-3 flex h-10 w-20 items-center justify-center rounded-md bg-mainColor p-1 cursor-pointer">뒤로가기</div>
-      </Link>
       <PostForm />
     </div>
   );
