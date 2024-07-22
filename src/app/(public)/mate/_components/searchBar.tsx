f76e2ea57b6cc86b2742894439452d8bdf5c07a4
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 02:03:02 +0900

    update: 검색 기능 추가
    아직 화면에 그려주는 부분 구현해야 함

---
+++ src/app/(public)/mate/_components/searchBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchData, setSearchData] = useState<any>([]);
+
+  const handleSearchPosts = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/api/mate/post?query=${searchQuery}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setSearchData(data); 
+ 
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  console.log('검색결과', searchData);
+  
+
+  return (
+    <div className="mb-5 flex justify-center">
+      <form onSubmit={handleSearchPosts} className="flex w-[300px] flex-row items-center rounded-full border p-1">
+        <input type="text" className="w-[270px]" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+        <button type="submit" className="ml-2">
+          🔍
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
