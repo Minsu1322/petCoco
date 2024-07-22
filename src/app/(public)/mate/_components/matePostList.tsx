f5d409e1deb87249f2f1a100c00c46f28d9353da
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 02:07:54 +0900

    fix: 모집 중인 메이트가 없을 경우 처리

--- src/app/(public)/mate/_components/matePostList.tsx
+++ src/app/(public)/mate/_components/matePostList.tsx
@@ -4,13 +4,13 @@ import MatePostItem from './matePostItem';
 
 
 interface MatePostListProps {
-  posts: MatePostFullType[]
+  posts: MatePostFullType[] | undefined;
 }
 
 const MatePostList = ({posts}: MatePostListProps) => {
   return (
     <div className="grid grid-cols-2 justify-items-center mt-5">
-      {posts.map((post) => <MatePostItem key={post.id} post={post} />)}
+      {posts ? posts.map((post) => <MatePostItem key={post.id} post={post} />) : (<div> 현재 모집 중인 산책 메이트가 없습니다.</div>)}
     </div>
   )
 }
