55b9004b809b0519166d3a8fa1aedc31339d0607
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 15:21:53 +0900

    fix: 경도위도, 주소 전역으로 관리하기 위해 수정

--- src/zustand/locationStore.ts
+++ src/zustand/locationStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-type Position = {
+export type Position = {
   center: {
     lat: number;
     lng: number;
@@ -11,16 +11,22 @@ type Position = {
 
 type UseLocationStore = {
   position: Position;
+  address: string;
   setPosition: (position: Partial<Position>) => void;
+  setAddress: (address: string) => void;
 };
 
 export const locationStore = create<UseLocationStore>((set) => ({
-  position: { 
-    center: { lat: 37.5556236021213, lng: 126.992199507869 }, 
-    errMsg: null, 
-    isLoading: true 
+  position: {
+    center: { lat: 37.5556236021213, lng: 126.992199507869 },
+    errMsg: null,
+    isLoading: true
   },
-  setPosition: (newPosition) => set((state) => ({ 
-    position: { ...state.position, ...newPosition } 
-  }))
+  setPosition: (newPosition) => {
+    return set((state) => ({
+      position: { ...state.position, ...newPosition }
+    }));
+  },
+  address: "서울특별시 중구 삼일대로 231",
+  setAddress: (address) => set({ address }),
 }));
