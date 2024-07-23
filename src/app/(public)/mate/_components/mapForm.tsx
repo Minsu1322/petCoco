9c49f64778cc6fd8b0d97c5a0d7009851211f9c0
hyeonw8 <whgusrud223@gmail.com>
2024-07-23 15:19:04 +0900

    add: 글쓰기에 사용되는 map 컴포넌트 수정
    브라우저에서 뒤로가기 시 기존 데이터가 남아있던 문제 해결

--- src/app/(public)/mate/_components/mapForm.tsx
+++ src/app/(public)/mate/_components/mapForm.tsx
@@ -5,30 +5,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 //import { useRef, useState } from "react";
 import { Map, MapMarker, MapTypeControl, ZoomControl } from "react-kakao-maps-sdk";
+import { getConvertAddress } from "./getConvertAddress";
 
 interface MapComponentProps {
   center: { lat: number; lng: number };
-  isEditing?: boolean;
-  dbPosition?: { lat: number; lng: number };
-  //markerPosition: { lat: number; lng: number };
 }
 
-const MapComponent = ({ center }: MapComponentProps) => {
-  const { position, setPosition, address, setAddress } = locationStore();
-  // const mapRef = useRef<kakao.maps.Map>(null)
-  // const [mapType, setMapType] = useState<"roadmap" | "skyview">("roadmap")
-
-  // const zoomIn = () => {
-  //   const map = mapRef.current
-  //   if (!map) return
-  //   map.setLevel(map.getLevel() - 1)
-  // }
-
-  // const zoomOut = () => {
-  //   const map = mapRef.current
-  //   if (!map) return
-  //   map.setLevel(map.getLevel() + 1)
-  // }
+const MapForm = ({ center }: MapComponentProps) => {
+  const { position, setPosition} = locationStore();
 
 
   const {
@@ -40,23 +24,11 @@ const MapComponent = ({ center }: MapComponentProps) => {
     queryFn: async () => {
       const response = await getConvertAddress(position.center);
       return response;
-    },
+    }, 
+    enabled: !!position.center,
   });
 
   console.log("주소 변환 데이터 확인", addressData);
-
-  // if(position.center && addressData) {
-  //   setAddress(addressData);
-  // }
-  // console.log('현재 위치 값', position);
-
-  useEffect(() => {
-    if (addressData) {
-      // addressData의 구조에 따라 적절히 수정해야 합니다.
-      // 예를 들어, addressData.address가 주소 문자열이라고 가정합니다.
-      setAddress(addressData.address);
-    }
-  }, [addressData]);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -70,30 +42,31 @@ const MapComponent = ({ center }: MapComponentProps) => {
             isLoading: false
           });
         },
-        (err) => {
+        (error) => {
           setPosition({
-            errMsg: err.message,
+            center: center,
+            errMsg: error.message,
             isLoading: false
           });
         }
       );
     }
-
     if (!navigator.geolocation) {
-      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용 설정
       setPosition({
         center: center,
         errMsg: "geolocation을 사용할수 없어요..",
         isLoading: false
       });
     }
-  }, []);
+    // console.log('geo',navigator);
 
+  
+  }, []);
 
   return (
     <>
       <Map
-        center={ position.center ?? center}
+        center={position.center}
         style={{ width: "500px", height: "360px" }}
         level={5}
         onClick={(_, mouseEvent) => {
@@ -105,22 +78,20 @@ const MapComponent = ({ center }: MapComponentProps) => {
             }
           });
         }}
-        // mapTypeId={mapType === "roadmap" ? "ROADMAP" : "HYBRID"}
-        // ref={mapRef}
       >
         <MapMarker
-          position={ position.center ?? center}
+          position={position.center}
           image={{
             src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN26a7CVa5ryzx5psOXRzK2a-OfomhbbUbw-zxRX7D835ImjsmTOc2tIgkc-LXQ2cFrf0&usqp=CAU",
             size: {
               width: 64,
               height: 69
-            }, // 마커이미지 크기
+            }, 
             options: {
               offset: {
                 x: 27,
                 y: 69
-              } // 마커이미지 옵션, 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
+              } 
             }
           }}
         >
@@ -133,4 +104,4 @@ const MapComponent = ({ center }: MapComponentProps) => {
   );
 };
 
-export default MapComponent;
+export default MapForm;
