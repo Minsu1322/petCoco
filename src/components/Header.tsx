96c61d38c5f953f69f9cb0f602f5c0998410fc88
hyeonw8 <whgusrud223@gmail.com>
2024-07-25 00:12:26 +0900

    css: mainColor, text 색상 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
   }, [setSession]);
 
   return (
-    <header className="mb-6 w-full items-center bg-[#1FE476] px-10 py-6 text-white">
+    <header className="mb-6 w-full items-center bg-[#1FE476] px-10 py-6 text-black">
       <div className="flex justify-center gap-6">
         <Link href={"/"}>
           <p>홈(임시 헤더)</p>
