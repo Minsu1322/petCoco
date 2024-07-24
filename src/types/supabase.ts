20b8be8d6dc4f45c27b2e341c16940350b8cab79
YUNSAERA <smctofk96@gmail.com>
2024-07-24 20:36:49 +0900

    add

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -89,42 +89,51 @@ export type Database = {
       }
       matePosts: {
         Row: {
+          characteristics: string | null
           content: string | null
           created_at: string
+          dateTime: string | null
           id: string
           male_female: string | null
           members: string | null
           neutered: boolean | null
           numbers: string | null
           position: Json | null
+          recruiting: boolean | null
           size: string | null
           title: string | null
           user_id: string | null
           weight: string | null
         }
         Insert: {
+          characteristics?: string | null
           content?: string | null
           created_at?: string
+          dateTime?: string | null
           id?: string
           male_female?: string | null
           members?: string | null
           neutered?: boolean | null
           numbers?: string | null
           position?: Json | null
+          recruiting?: boolean | null
           size?: string | null
           title?: string | null
           user_id?: string | null
           weight?: string | null
         }
         Update: {
+          characteristics?: string | null
           content?: string | null
           created_at?: string
+          dateTime?: string | null
           id?: string
           male_female?: string | null
           members?: string | null
           neutered?: boolean | null
           numbers?: string | null
           position?: Json | null
+          recruiting?: boolean | null
           size?: string | null
           title?: string | null
           user_id?: string | null
@@ -146,6 +155,7 @@ export type Database = {
           content: string | null
           created_at: string
           id: string
+          post_imageURL: string | null
           title: string | null
           user_id: string | null
         }
@@ -154,6 +164,7 @@ export type Database = {
           content?: string | null
           created_at?: string
           id?: string
+          post_imageURL?: string | null
           title?: string | null
           user_id?: string | null
         }
@@ -162,6 +173,7 @@ export type Database = {
           content?: string | null
           created_at?: string
           id?: string
+          post_imageURL?: string | null
           title?: string | null
           user_id?: string | null
         }
