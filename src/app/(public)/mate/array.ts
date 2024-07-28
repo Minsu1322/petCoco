d00d8415168cd6ba37ea0099051e065743fcc6c4
hyeonw8 <whgusrud223@gmail.com>
2024-07-28 20:29:19 +0900

    update: 필터 설정 값 array 추가

--- src/app/(public)/mate/array.ts
+++ src/app/(public)/mate/array.ts
@@ -1,1 +1,31 @@
-export const characteristicsArr = ["선택", "온순함", "활발함", "소심함", "적극적", "외향적", "내향적", "낯가림"]
+export const characteristicsArr = ["선택", "온순함", "활발함", "소심함", "적극적", "외향적", "내향적", "낯가림"];
+
+export const gender = [
+  {key: "남", label: "남"},
+  {key: "여", label: "여"},
+];
+
+export const ageRange = [
+  {key: "10", label: "10"},
+  {key: "20", label: "20"},
+  {key: "30", label: "30"},
+  {key: "40", label: "40"},
+  {key: "50", label: "50"},
+  {key: "60", label: "60"},
+  {key: "70", label: "70"},
+];
+
+export const location = [
+  {key: "0", label: "0"},
+  {key: "5", label: "5"},
+  {key: "10", label: "10"},
+  {key: "15", label: "15"},
+  {key: "30", label: "30"},
+  {key: "50", label: "50"},
+  {key: "100", label: "100"},
+];
+
+export const petGender = [
+  {key: "남", label: "남"},
+  {key: "여", label: "여"},
+];
