2fb074cea26b85be72bf0d99cba169cb6bc86900
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 09:28:56 +0900

    fix: 필터 컴포넌트 밖으로 꺼내서 우선 구현하기 위해 코드 이동

--- src/app/(public)/mate/page.tsx
+++ src/app/(public)/mate/page.tsx
@@ -10,10 +10,12 @@ import PostItemFilterTab from "./_components/postItemFilterTab";
 // import SearchBar from "./_components/searchBar";
 import FilterSelectChip from "./_components/filterSelectChip";
 import FilterDateChip from "./_components/filterDateChip";
-import { getDistanceHaversine } from "./getDistanceHaversine";
 
 import { useAuthStore } from "@/zustand/useAuth";
 import NotLogInView from "./_components/notLogInView";
+import FilterWeightChip from "./_components/filterWeightChip";
+import { age, gender, male_female, position } from "./array";
+import { DateValue } from "@nextui-org/react";
 
 export type PositionData = {
   center: {
@@ -42,6 +44,7 @@ const MatePage = () => {
     male_female: null,
     weight: null,
   });
+  const [date_time, setDate_time] = useState<DateValue | null>(null);
 
   const updateFilter = (filterName: string, value: any) => {
     setFilters((prevFilters) => ({
@@ -113,7 +116,45 @@ const MatePage = () => {
             </form>
           </div>
           {/* <PostItemFilterTab updateFilter={updateFilter} filters={filters} /> */}
-          <PostItemFilterTab handleDistanceFilter={handleDistanceFilter}  updateFilter={updateFilter} filters={filters}  />
+          {/* <PostItemFilterTab handleDistanceFilter={handleDistanceFilter}  updateFilter={updateFilter} filters={filters}  /> */}
+          <div className="w-full">
+      <div>
+        <p className="text-lg">메이트 상세 필터</p>
+        <FilterSelectChip
+          label="성별"
+          array={gender}
+          selected={filters.gender}
+          onSelect={(items) => updateFilter("gender", items)}
+        />
+        <FilterSelectChip
+          label="연령대"
+          array={age}
+          selected={filters.age}
+          onSelect={(items) => updateFilter("age", items)}
+        />
+        <FilterDateChip 
+          label="산책일" 
+          selectedDate={date_time} 
+          onDateChange={setDate_time} 
+        />  
+        <FilterSelectChip
+          label="거리"
+          array={position}
+          selected={filters.position}
+          onSelect={(items) => updateFilter("position", items)}
+        />
+      </div>
+      <div className="mt-5">
+        <p className="text-lg">반려견 정보 필터</p>
+        <FilterWeightChip label="반려견 몸무게" />
+        <FilterSelectChip
+          label="성별"
+          array={male_female}
+          selected={filters.male_female}
+          onSelect={(items) => updateFilter("male_female", items)}
+        />
+      </div>
+    </div>
           <div className="mt-5 flex">
               <div className="mb-4 h-10 w-full items-center rounded-lg bg-gray-300 p-2 text-center" onClick={handleResetFilter}>초기화</div>
           </div>
