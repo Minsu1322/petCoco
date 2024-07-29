b0124feb894cac7c35b6d28170015739a20f15fb
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 17:55:57 +0900

    fix: 사용하지 않는 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useAuthStore } from "@/zustand/useAuth";
-
 export default function Home() {
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">...</main>;
 }
