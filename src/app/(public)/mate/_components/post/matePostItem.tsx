9f31bfa34f0ebe7f7de6c5c16adedfbabb69d921
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 09:13:40 +0900

    fix: 게시글 아이템에 들어가는 데이터 수정

--- src/app/(public)/mate/_components/post/matePostItem.tsx
+++ src/app/(public)/mate/_components/post/matePostItem.tsx
@@ -33,49 +33,47 @@ const MatePostItem = ({ post }: MatePostItemPorps) => {
   //console.log(post);
 
   return (
-    <Link href={`/mate/posts/${post.id}`} className="mb-5 flex w-11/12 flex-col gap-y-5 rounded-xl bg-gray-200 p-5">
+    <Link href={`/mate/posts/${post.id}`} className="mb-5 mr-2 flex flex-col rounded-xl bg-gray-200 p-5">
       <div className="flex flex-row gap-x-5">
         <div className="ml-1">
           <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN26a7CVa5ryzx5psOXRzK2a-OfomhbbUbw-zxRX7D835ImjsmTOc2tIgkc-LXQ2cFrf0&usqp=CAU"
             alt="사용자 프로필 이미지"
-            width={70}
-            height={70}
+            width={125}
+            height={125}
           />
-          <p className="mt-2 w-16 overflow-hidden overflow-ellipsis whitespace-nowrap text-center">
-            {" "}
-            {post && post.users?.nickname}
-          </p>
+          {/* users 이미지 구현되면, users의 profileUrl 받아와야 함 */}
         </div>
         <div className="w-full">
-          <div className="flex flex-row justify-between">
-            <p className="w-52 overflow-hidden overflow-ellipsis whitespace-nowrap">
-              {`${extractDong(post.address || "")}, ${post.place_name || ""}`}
-            </p>
-            {distance !== null ? <p>현재 위치에서의 거리: {distance} km</p> : <p></p>}
+          <div className="flex flex-col justify-between">
+            <div className="mb-1 flex flex-row gap-x-2">
+              <ItemButton
+                text={`${Array.isArray(post.matePostPets) ? ` ${post.matePostPets.length} 마리` : ""}`}
+                className="flex h-7 w-20 items-center justify-center rounded-full bg-white"
+              />
+              <ItemButton
+                text={post.users?.mbti}
+                className="flex h-7 w-20 items-center justify-center rounded-full bg-white px-2"
+                p_className="w-17 overflow-hidden overflow-ellipsis whitespace-nowrap"
+              />
+              <ItemButton
+                text={Number(post.members) > 1 ? "다인원 산책" : "소인원 산책"}
+                className="flex h-7 w-24 items-center justify-center rounded-full bg-white"
+              />
+            </div>
+            <div className="mt-1">
+              {distance !== null ? <p className="text-gray-700">🎡 현재 위치에서의 거리: {distance} km</p> : <p></p>}
+              <p className="w-52 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                {`${extractDong(post.address || "")}, ${post.place_name || ""}`}
+              </p>
+            </div>
           </div>
           {/* <p>{post.content}</p> */}
-          <p>날짜 : {post.date_time?.split("T")[0]}</p>
-          <p>시간 : {getConvertTime({ date_time: post.date_time || "" })}</p>
-          <p>모집인원 수 {post.members}</p>
-          <div className="mt-2 flex flex-row gap-x-2">
-            <ItemButton
-              text={`${Array.isArray(post.matePostPets) ?` ${post.matePostPets.length} 마리` : ""}`}
-              className="flex h-7 w-20 items-center justify-center rounded-full bg-white"
-            />
-            <ItemButton
-              text={post.matePostPets[0]?.characteristics}
-              className="flex h-7 w-20 items-center justify-center rounded-full bg-white px-2"
-              p_className="w-17 overflow-hidden overflow-ellipsis whitespace-nowrap"
-            />
-            <ItemButton
-              text={Number(post.members) > 1 ? "다인원 산책" : "소인원 산책"}
-              className="flex h-7 w-24 items-center justify-center rounded-full bg-white"
-            />
-          </div>
+          <p>{post.date_time?.split("T")[0]} | {getConvertTime({ date_time: post.date_time || "" })}</p>
+          <p>{post.members}명 모집</p>
         </div>
       </div>
-      <div className="flex flex-row justify-end gap-x-3">
+      <div className="mt-2 flex flex-row justify-end gap-x-3">
         <ItemButton
           text={post.recruiting ? "모집 중" : "모집 완료"}
           className="w-24 rounded-full bg-white p-3 text-center"
