b5f3a322738d7ba6681e8523099d2e5ad5ae7ee8
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 09:07:40 +0900

    fix: 셀렉트 필터 수정

--- src/app/(public)/mate/_components/filterSelectChip.tsx
+++ src/app/(public)/mate/_components/filterSelectChip.tsx
@@ -1,29 +1,33 @@
-import {Select, SelectSection, SelectItem} from "@nextui-org/react";
-import { gender } from "../array";
+import { Select, SelectSection, SelectItem } from "@nextui-org/react";
 
 interface FilterSelectChipProps {
   label: string;
   array: Array<{ key: string; label: string }>;
+  onSelect: (value: string) => void; 
+  selected: string | null;
 }
 
-const FilterSelectChip = ({ label, array}: FilterSelectChipProps) => {
+const FilterSelectChip = ({ label, array, onSelect, selected }: FilterSelectChipProps) => {
   return (
     <div className="flex w-full mt-5">
       <Select 
-        size="sm"
-        radius="full"
-        label={label}
-        color="default"
-        className="max-w-xs w-full" 
-      >
-        {array.map((arr) => (
-          <SelectItem key={arr.key}>
-            {arr.label}
-          </SelectItem>
-        ))}
-      </Select>
+      size="sm" 
+      radius="full" 
+      label={label} 
+      color="default" 
+      className="w-full max-w-xs"
+      selectedKeys={selected ? [selected] : []}
+      onSelectionChange={(keys) => {
+        const selectedValue = Array.from(keys)[0] as string;
+        onSelect(selectedValue);
+      }}
+    >
+      {array.map((arr) => (
+        <SelectItem key={arr.key} value={arr.key}>{arr.label}</SelectItem>
+      ))}
+    </Select>
     </div>
-  )
+  );
 };
 
 export default FilterSelectChip;
