692313063c48f79ce17e870b1f0bf1a86f4f5f06
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 09:14:38 +0900

    fix: 메이트, 반려견 필터 탭 컴포넌트 수정

--- src/app/(public)/mate/_components/postItemFilterTab.tsx
+++ src/app/(public)/mate/_components/postItemFilterTab.tsx
@@ -1,20 +1,65 @@
+"use client";
+
 import FilterSelectChip from "./filterSelectChip";
 import FilterDateChip from "./filterDateChip";
-import { gender, ageRange, location, petGender } from "../array";
+import { gender, age, position, male_female } from "../array";
+import { DateValue } from "@nextui-org/react";
+import { useState } from "react";
+import FilterWeightChip from "./filterWeightChip";
 
-const PostItemFilterTab = () => {
+interface PostItemFilterTabProps {
+  updateFilter: (filterName: string, value: any) => void;
+  filters: {
+    gender: string | null,
+    date_time: string | null,
+    position: string | null,
+    male_female: string | null,
+    age: string | null,
+    weight: string | null,
+  };
+  handleDistanceFilter: () => void;
+}
+
+const PostItemFilterTab = ({ updateFilter, filters }: PostItemFilterTabProps) => {
+  const [date_time, setDate_time] = useState<DateValue | null>(null);
+
   return (
     <div className="w-full">
       <div>
         <p className="text-lg">메이트 상세 필터</p>
-        <FilterSelectChip label="성별" array={gender} />
-        <FilterSelectChip label="연령대" array={ageRange} />
-        <FilterDateChip label="산책일" />
-        <FilterSelectChip label="거리" array={location} />
+        <FilterSelectChip
+          label="성별"
+          array={gender}
+          selected={filters.gender}
+          onSelect={(items) => updateFilter("gender", items)}
+        />
+        <FilterSelectChip
+          label="연령대"
+          array={age}
+          selected={filters.age}
+          onSelect={(items) => updateFilter("age", items)}
+        />
+        <FilterDateChip 
+          label="산책일" 
+          selectedDate={date_time} 
+          onDateChange={setDate_time} 
+        />  
+        <FilterSelectChip
+          label="거리"
+          array={position}
+          selected={filters.position}
+          onSelect={(items) => updateFilter("position", items)}
+        />
       </div>
       <div className="mt-5">
         <p className="text-lg">반려견 정보 필터</p>
-        <FilterSelectChip label="성별" array={petGender} />
+        <FilterWeightChip label="반려견 몸무게" />
+        <FilterSelectChip
+          label="성별"
+          array={male_female}
+          selected={filters.male_female}
+          onSelect={(items) => updateFilter("male_female", items)}
+        />
       </div>
     </div>
   );
