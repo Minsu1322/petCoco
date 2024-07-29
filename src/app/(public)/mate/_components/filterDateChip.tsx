cd1b7e36f0f5082d2523e2750a4d60185a168ebf
hyeonw8 <whgusrud223@gmail.com>
2024-07-29 15:27:05 +0900

    fix: filter 로직 수정

--- src/app/(public)/mate/_components/filterDateChip.tsx
+++ src/app/(public)/mate/_components/filterDateChip.tsx
@@ -2,20 +2,21 @@ import { DatePicker, DateValue, Input } from "@nextui-org/react";
 
 interface FilterDateChipProps {
   label: string;
-  selectedDate: DateValue | null;
-  onDateChange: (value: DateValue | null) => void;
+  selected: DateValue | null;
+  onSelect: (value: DateValue | null) => void;
 }
 
-const FilterDateChip = ({ label, selectedDate, onDateChange }: FilterDateChipProps) => {
+const FilterDateChip = ({ label, onSelect, selected }: FilterDateChipProps) => {
   return (
     <DatePicker
       label={label}
       className="mt-5 w-full max-w-xs"
       radius="full"
-      value={selectedDate}
-      onChange={onDateChange}
+      value={selected}
+      onChange={onSelect}
     />
   );
 };
 
 export default FilterDateChip;
+  
--- src/app/(public)/mate/_components/filterSelectChip.tsx
+++ src/app/(public)/mate/_components/filterSelectChip.tsx
@@ -3,29 +3,28 @@ import { Select, SelectSection, SelectItem } from "@nextui-org/react";
 interface FilterSelectChipProps {
   label: string;
   array: Array<{ key: string; label: string }>;
-  onSelect: (value: string) => void; 
+  onSelect: (value: string) => void;
   selected: string | null;
 }
 
 const FilterSelectChip = ({ label, array, onSelect, selected }: FilterSelectChipProps) => {
   return (
-    <div className="flex w-full mt-5">
-      <Select 
-      size="sm" 
-      radius="full" 
-      label={label} 
-      color="default" 
-      className="w-full max-w-xs"
-      selectedKeys={selected ? [selected] : []}
-      onSelectionChange={(keys) => {
-        const selectedValue = Array.from(keys)[0] as string;
-        onSelect(selectedValue);
-      }}
-    >
-      {array.map((arr) => (
-        <SelectItem key={arr.key} value={arr.key}>{arr.label}</SelectItem>
-      ))}
-    </Select>
+    <div className="mt-5 flex w-full">
+      <Select
+        size="sm"
+        radius="full"
+        label={label}
+        color="default"
+        className="w-full max-w-xs"
+        value={selected || ""}
+        onChange={(e) => onSelect(e.target.value || "")}
+      >
+        {array.map((arr) => (
+          <SelectItem key={arr.key} value={arr.key}>
+            {arr.label}
+          </SelectItem>
+        ))}
+      </Select>
     </div>
   );
 };
