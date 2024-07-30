7e6207b1d9066f94b621f04d9570ce2b78af9f4c
hyeonw8 <whgusrud223@gmail.com>
2024-07-31 00:12:11 +0900

    css: 메인 컬러 설정 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
       colors: {
-        mainColor: "#1FE476"
+        mainColor: "#8CF2B7"
       }
     }
   },
