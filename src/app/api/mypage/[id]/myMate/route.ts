e67e1bf71b07151ebacaf447b28f44846c33ae04
hyeonw8 <whgusrud223@gmail.com>
2024-07-30 17:04:00 +0900

    add: 마이페이지 활동 내역 라우터 핸들러 추가

---
+++ src/app/api/mypage/[id]/myMate/route.ts
@@ -0,0 +1,24 @@
+import { createClient } from "@/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const supabase = createClient();
+  const { id } = params;
+
+  try {
+    const { data, error } = await supabase
+      .from("matePosts")
+      .select("*")
+      .eq('id', id)
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ err }, { status: 500 });
+  }
+};
