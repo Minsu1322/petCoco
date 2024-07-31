a0d8b97c801fcba4f10fa7c535ffa274926a4f5c
FE_sunmin <fe.sunmin@gmail.com>
2024-07-31 16:19:30 +0900

    de: 소켓 실시간 채팅 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-"use client";
-
-import Chat from "@/components/chat/Chat";
-import { useState } from "react";
-import { RiChat1Fill } from "react-icons/ri";
-
 export default function Home() {
-  const [isChatOpen, setIsChatOpen] = useState(false);
-
-  const toggleChat = () => {
-    setIsChatOpen(!isChatOpen);
-  };
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <button
-          onClick={toggleChat}
-          className="fixed bottom-4 right-4 rounded-full border border-gray-300 bg-white p-2 shadow-lg focus:outline-none"
-        >
-          <RiChat1Fill />
-        </button>
-        <Chat isOpen={isChatOpen} onClose={toggleChat} />
-      </div>
-    </main>
-  );
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">...</main>;
 }
