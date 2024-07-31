c583c5ccba5102f6ac1101934369b5c81635e0ac
hyeonw8 <whgusrud223@gmail.com>
2024-08-01 00:12:00 +0900

    ifx: 시간 변환 함수 수정

--- src/app/utils/getConvertTime.ts
+++ src/app/utils/getConvertTime.ts
@@ -3,10 +3,12 @@ interface GetConvertTimeProps {
 }
 
 export const getConvertTime = ({date_time}: GetConvertTimeProps) => {
-  const time = date_time?.split("T")[1].split(':');
-  const convertPeriod  = (Number(time[0]) < 12 ? '오전' : '오후')
+  const time = date_time?.split("T")[1]?.split(':') ?? [];
+  if (time.length < 2) return '시간 정보 없음';
+
+  const convertPeriod = (Number(time[0]) < 12 ? '오전' : '오후');
   const convertHour = (Number(time[0]) % 12 || 12);
   const convertMin = time[1];
 
-  return `${convertPeriod} ${convertHour}시 ${convertMin}분`
-}
+  return `${convertPeriod} ${convertHour}:${convertMin}`;
+};
