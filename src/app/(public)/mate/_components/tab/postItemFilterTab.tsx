68841f1bb9578d7ed75c3b86d56838263a8ec0e9
hyeonw8 <whgusrud223@gmail.com>
2024-08-13 02:51:15 +0900

    fix: filter 조건 추가

--- src/app/(public)/mate/_components/tab/postItemFilterTab.tsx
+++ src/app/(public)/mate/_components/tab/postItemFilterTab.tsx
@@ -16,6 +16,7 @@ interface PostItemFilterTabProps {
     weight: string | null;
     regions: string | null;
     times: string | null;
+    neutered: string | null;
   };
   onClick: () => void;
 }
