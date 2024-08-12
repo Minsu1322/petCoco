b8b4313fc7cd762292cca93b8653bd58bd89669b
YUNSAERA <smctofk96@gmail.com>
2024-08-13 00:35:38 +0900

    updata:병합

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className={`${inter.className} flex min-h-screen flex-col bg-white`}>
-=======
-      <body className={`${inter.className} flex min-h-screen flex-col`}>
->>>>>>> dev
         <QueryProvider>
           <Header />
           <main className="mx-auto w-full flex-grow">{children}</main>
