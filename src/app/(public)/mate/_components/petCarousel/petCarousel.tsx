86d4a958b9474aa8adb8d764ea68477e2e820f73
hyeonw8 <whgusrud223@gmail.com>
2024-08-14 14:37:43 +0900

    fix: 캐러셀 적용 및 수정

--- src/app/(public)/mate/_components/petCarousel/petCarousel.tsx
+++ src/app/(public)/mate/_components/petCarousel/petCarousel.tsx
@@ -1,72 +1,61 @@
-import React, { useCallback } from 'react';
-import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel';
-import { PetCarouselButton, useDotButton } from './petCarouselButton';
-import Autoplay from 'embla-carousel-autoplay';
-import useEmblaCarousel from 'embla-carousel-react';
-import styles from '../petCarousel/styles/css/petCarousel.module.css';
+import React, { useCallback, useEffect } from "react";
+import { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
+import { DotButton, useDotButton } from "./components/MyPetCarouselDotButtons";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import styles from "../petCarousel/styles/css/petCarousel.module.css";
 
 import { matepostpetsType } from "@/types/mate.type";
-import PetItem from '../../posts/_components/petItem';
+import PetItem from "../../posts/_components/petItem";
 
 type PropType = {
   pets: matepostpetsType[];
   slides: number[];
   options?: EmblaOptionsType;
-}
+};
 
 const PetCarousel: React.FC<PropType> = (props) => {
   const { slides, options, pets } = props;
 
   const autoplayOptions = {
-    delay: 10000, // 3초 주기
+    delay: 10000,
     stopOnInteraction: false
   };
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay(autoplayOptions)])
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay(autoplayOptions)]);
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
 
-  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
-    const autoplay = emblaApi?.plugins()?.autoplay
-    if (!autoplay) return
+  const handleSelect = useCallback(() => {
+    if (emblaApi) {
+      const selectedIndex = emblaApi.selectedScrollSnap();
+      emblaApi.scrollTo(selectedIndex);
+    }
+  }, [emblaApi]);
 
-    const resetOrStop =
-      autoplay.options.stopOnInteraction === false
-        ? autoplay.reset
-        : autoplay.stop
-
-    resetOrStop()
-  }, [])
-
-  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
-    emblaApi,
-    onNavButtonClick
-  )
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on("select", handleSelect);
+    }
+  }, [emblaApi, handleSelect]);
 
   return (
-    <section className={styles.embla}>
-      <div className={styles.embla} ref={emblaRef}>
+    <section className={`${styles.embla}`}>
+      <div className={styles.embla__viewport} ref={emblaRef}>
         <div className={styles.embla__container}>
           {pets.map((pet) => (
-            <div key={pet.id} className={styles.embla__slide}>
-              <PetItem pet={pet} />
+           <div key={pet.id} className={`${styles.embla__slide}`}>
+            <PetItem pet={pet} />
             </div>
           ))}
         </div>
       </div>
 
-      <div className={styles.embla__controls}>
-        <div className={styles.embla__dots}>
-          {scrollSnaps.map((_, index) => (
-            <PetCarouselButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={`${styles.embla__dot} ${
-                index === selectedIndex ? styles['embla__dot--selected'] : ''
-              }`}
-            />
-          ))}
-        </div>
+      <div className={`${styles.embla__dots}`}>
+        {scrollSnaps.map((_, index) => (
+          <DotButton key={index} onClick={() => onDotButtonClick(index)} selected={index === selectedIndex} />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default PetCarousel;
