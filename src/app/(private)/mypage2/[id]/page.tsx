0ab0c2e5efe65a4204cfab54870bcf4056ac92e6
FE_sunmin <fe.sunmin@gmail.com>
2024-08-17 20:26:30 +0900

    fix: 구현 안된 기능 주석처리

--- src/app/(private)/mypage2/[id]/page.tsx
+++ src/app/(private)/mypage2/[id]/page.tsx
@@ -65,24 +65,6 @@ function MyPage() {
     queryFn: getPetData
   });
 
-<<<<<<< HEAD
-  const { data: myMate } = useQuery<MatePostType[]>({
-    queryKey: ["myMate", id],
-    queryFn: async () => {
-      const response = await fetch(`/api/mate/my/${id}`);
-      const data = response.json();
-      return data;
-    },
-    enabled: !!id
-  });
-
-  if (!id) {
-    return;
-  } else {
-    const recruitingTrueCount = myMate?.filter((post) => post.recruiting === true).length || 0;
-    const recruitingFalseCount = myMate?.filter((post) => post.recruiting === false).length || 0;
-  }
-=======
   // const { data: myMate } = useQuery<MatePostType[]>({
   //   queryKey: ["myMate", id],
   //   queryFn: async () => {
@@ -101,7 +83,6 @@ function MyPage() {
       router.push(`/signin`);
     }
   }, [id]);
->>>>>>> dev
 
   if (isPending || isPetPending) {
     return (
@@ -168,9 +149,9 @@ function MyPage() {
         </div>
       </div>
 
-      <div className="flex w-full items-center px-[24px] pt-[16px]">
+      {/* <div className="flex w-full items-center px-[24px] pt-[16px]">
         <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">나의 산책</div>
-      </div>
+      </div> */}
 
       {/* <div className="border-b-1 px-[14px] py-4">
         <div>
