3ee6ac8fcee7369190aae3d5ed13b959db06a0c7
airtrader <hosi2714@naver.com>
2024-08-19 12:05:55 +0900

    update: 유저 정보 확인페이지 완성

--- src/app/(public)/userInfo/[id]/page.tsx
+++ src/app/(public)/userInfo/[id]/page.tsx
@@ -1,27 +1,29 @@
 "use client";
 
 import { UsersPetType } from "@/types/auth.type";
+import { MatePostType } from "@/types/mate.type";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { defaultUserImg, defaultPetImg } from "@/components/DefaultImg";
 import { EmblaOptionsType } from "embla-carousel";
-import MyPetCarousel from "./../MyPetCarousel/MyPetCarousel";
+
 import Image from "next/image";
 import { useEffect } from "react";
-import LogoutButton from "../_components/LogoutBtn";
+
 import LoadingComponent from "@/components/loadingComponents/Loading";
+import MyPetCarousel from "../MyPetCarousel/MyPetCarousel";
 
 type PetType = UsersPetType;
 
-function MyPage() {
+function UserInfoPage() {
   const params = useParams();
   const router = useRouter();
   let id = params?.id || 0;
   id = id === "undefined" ? 0 : id;
 
   if (!id) {
-    router.push(`/signin`);
+    router.push(`/`);
   }
 
   const getProfileData = async () => {
@@ -79,7 +81,7 @@ function MyPage() {
 
   useEffect(() => {
     if (!id) {
-      router.push(`/signin`);
+      router.push(`/`);
     }
   }, [id]);
 
@@ -118,28 +120,24 @@ function MyPage() {
             {user.nickname}
           </div>
         </div>
-        <Link href={`/mypage2/${user.id}/fixMyProfile`}>
+        <Link href={`/userInfo/${user.id}/userProfile`}>
           <div className="mb-[13px] mt-[15px] whitespace-nowrap rounded-lg bg-[#8E6EE8] px-4 py-[6px] text-xs font-semibold leading-relaxed tracking-wide text-[#FFFFFF]">
-            내 프로필 수정
+            유저 프로필 확인
           </div>
         </Link>
       </div>
 
       <div className="w-full border-b-1 px-[14px] py-[15px]">
         <div className="flex w-full flex-row items-center justify-between px-[12px]">
-          <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">나의 반려동물 ({pets?.length})</div>
+          <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">유저의 반려동물 ({pets?.length})</div>
           {pets && pets.length ? (
-            <Link href={`/mypage2/${user.id}/fixMyPetProfile/${pets[0]?.id}`}>
+            <Link href={`/userInfo/${user.id}/petProfile/${pets[0]?.id}`}>
               <button className="ml-2 self-stretch rounded-lg bg-[#8E6EE8] px-4 py-[6px] text-sm font-semibold text-[#FFFFFF]">
-                반려동물 프로필 수정
+                반려동물 프로필 확인
               </button>
             </Link>
           ) : (
-            <Link href={`/mypage2/${user.id}/addMyPetProfile`}>
-              <button className="ml-2 rounded-lg bg-[#8E6EE8] px-4 py-[6px] text-sm font-semibold tracking-wide text-[#FFFFFF]">
-                반려동물 프로필 추가
-              </button>
-            </Link>
+            <></>
           )}
         </div>
 
@@ -149,7 +147,7 @@ function MyPage() {
       </div>
 
       {/* <div className="flex w-full items-center px-[24px] pt-[16px]">
-        <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">나의 산책</div>
+        <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">유저의 산책</div>
       </div> */}
 
       {/* <div className="border-b-1 px-[14px] py-4">
@@ -173,12 +171,12 @@ function MyPage() {
 
       <div className="w-full px-[14px] py-[16px]">
         <div className="flex w-full items-center px-[12px]">
-          <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">나의 활동</div>
+          <div className="text-lg font-bold leading-[23.4px] text-[#3e3e3e]">유저의 활동</div>
         </div>
         <div className="my-[16px] flex w-full flex-col rounded-lg bg-[#EFEFF0] px-[8px] pt-[8px]">
-          <Link href={`/mypage2/${id}/myPosts`}>
+          <Link href={`/userInfo/${user.id}/userPosts`}>
             <div className="flex items-center justify-between border-b-1 px-[16px] py-[12px]">
-              <div className="text-[16px] font-normal leading-5 text-[#61646B]">내 포스트</div>
+              <div className="text-[16px] font-normal leading-5 text-[#61646B]">유저의 포스트</div>
               <svg width="6" height="12" viewBox="0 0 6 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M0.313439 11.0201C0.135928 10.8425 0.11979 10.5648 0.265027 10.369L0.313439 10.313L4.62633 5.99984L0.313439 1.68672C0.135928 1.50921 0.11979 1.23144 0.265027 1.0357L0.313439 0.979617C0.49095 0.802106 0.768726 0.785969 0.964467 0.931205L1.02055 0.979617L5.68721 5.64628C5.86472 5.82379 5.88086 6.10157 5.73562 6.29731L5.68721 6.35339L1.02055 11.0201C0.825283 11.2153 0.508701 11.2153 0.313439 11.0201Z"
@@ -187,9 +185,9 @@ function MyPage() {
               </svg>
             </div>
           </Link>
-          <Link href={`/mypage2/${id}/myMatePosts`}>
+          <Link href={`/userInfo/${user.id}/userMatePosts`}>
             <div className="flex items-center justify-between border-b-1 px-[16px] py-[12px] leading-5">
-              <div className="text-[16px] font-normal leading-5 text-[#61646B]">내 산책메이트</div>
+              <div className="text-[16px] font-normal leading-5 text-[#61646B]">유저의 산책메이트</div>
               <svg width="6" height="12" viewBox="0 0 6 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M0.313439 11.0201C0.135928 10.8425 0.11979 10.5648 0.265027 10.369L0.313439 10.313L4.62633 5.99984L0.313439 1.68672C0.135928 1.50921 0.11979 1.23144 0.265027 1.0357L0.313439 0.979617C0.49095 0.802106 0.768726 0.785969 0.964467 0.931205L1.02055 0.979617L5.68721 5.64628C5.86472 5.82379 5.88086 6.10157 5.73562 6.29731L5.68721 6.35339L1.02055 11.0201C0.825283 11.2153 0.508701 11.2153 0.313439 11.0201Z"
@@ -199,13 +197,9 @@ function MyPage() {
             </div>
           </Link>
         </div>
-      </div>
-
-      <div className="w-full px-[16px]">
-        <LogoutButton />
       </div>
     </div>
   );
 }
 
-export default MyPage;
+export default UserInfoPage;
