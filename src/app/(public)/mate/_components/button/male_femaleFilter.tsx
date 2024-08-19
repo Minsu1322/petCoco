8d5307d6795cbc7ed4ddbc335603325e7a95cd8f
hyeonw8 <whgusrud223@gmail.com>
2024-08-19 09:20:33 +0900

    css: 버튼 간격 수정

--- src/app/(public)/mate/_components/button/male_femaleFilter.tsx
+++ src/app/(public)/mate/_components/button/male_femaleFilter.tsx
@@ -20,15 +20,15 @@ const Male_femaleFilter = ({onSelect, selectedGender, setSelectedGender }: Male_
   return (
     <div className="w-full mb-[1.5rem]">
     <p className="mb-[0.69rem] text-[1rem] font-[400]">성별</p>
-    <div className="flex justify-between">
+    <div className="flex gap-x-[0.69rem]">
       <div
-        className={`px-[4.06rem] py-[1rem] rounded-[1rem] text-[1rem] cursor-pointer  font-[500]  ${selectedGender === "수" ? 'bg-mainColor text-white ' : 'bg-[#EFEFF0] text-[#999]' }`}
+        className={`px-[4.06rem] py-[1rem] flex justify-center rounded-[1rem] text-[1rem] cursor-pointer w-full font-[500]  ${selectedGender === "수" ? 'bg-mainColor text-white ' : 'bg-[#EFEFF0] text-[#999]' }`}
         onClick={() => handleSelect('수')}
       >
         남아
       </div>
       <div
-        className={`px-[4.06rem] py-[1rem] rounded-[1rem] text-[1rem] cursor-pointer text-[#999] font-[500]  ${selectedGender === "암" ? 'bg-mainColor text-white' : 'bg-[#EFEFF0] text-[#999]' }`}
+        className={`px-[4.06rem] py-[1rem] flex justify-center rounded-[1rem] text-[1rem] cursor-pointer w-full text-[#999] font-[500]  ${selectedGender === "암" ? 'bg-mainColor text-white' : 'bg-[#EFEFF0] text-[#999]' }`}
         onClick={() => handleSelect('암')}
       >
         여아
--- src/app/(public)/mate/_components/button/neuteredFilter.tsx
+++ src/app/(public)/mate/_components/button/neuteredFilter.tsx
@@ -17,12 +17,12 @@ const NeuteredFilter = ({ onSelect, selectedNeutered, setSelectedNeutered }: Neu
   };
 
   return (
-    <div className="w-full mb-[1.5rem]">
+    <div className="w-full mb-[1.5rem] ">
       <p className="mb-[0.69rem] text-[1rem] font-[400]">중성화 여부</p> 
     
-    <div className="flex justify-between">
+    <div className="flex gap-x-[1rem]">
       <div
-        className={`px-[1.66rem]  py-[1rem] rounded-[1rem] text-[1rem] text-[#999] font-[500] tracking-[0.0125rem] cursor-pointer ${
+        className={`px-[1.66rem]  py-[1rem] flex justify-center rounded-[1rem] text-[1rem] w-full text-[#999] font-[500] tracking-[0.0125rem] cursor-pointer ${
           selectedNeutered === "YES" ? 'bg-mainColor text-white ' : 'bg-[#EFEFF0] text-[#999]'
         }`}
         onClick={() => handleSelect("YES")}
@@ -30,7 +30,7 @@ const NeuteredFilter = ({ onSelect, selectedNeutered, setSelectedNeutered }: Neu
         했어요 
       </div>
       <div
-        className={`px-[1.25rem] py-[1rem] rounded-[1rem] text-[1rem] text-[#999]  font-[500] tracking-[0.0125rem] cursor-pointer ${
+        className={`px-[1.25rem] py-[1rem] flex justify-center rounded-[1rem] text-[1rem] w-full text-[#999]  font-[500] tracking-[0.0125rem] cursor-pointer ${
           selectedNeutered === "NO" ? 'bg-mainColor text-white ' : 'bg-[#EFEFF0] text-[#999]'
         }`}
         onClick={() => handleSelect("NO")}
@@ -38,7 +38,7 @@ const NeuteredFilter = ({ onSelect, selectedNeutered, setSelectedNeutered }: Neu
         안 했어요
       </div>
       <div
-        className={`px-[1.25rem] py-[1rem] rounded-[1rem] text-[1rem] text-[#999] font-[500] tracking-[0.0125rem] cursor-pointer ${
+        className={`px-[1.25rem] py-[1rem] flex justify-center rounded-[1rem] text-[1rem]  w-full text-[#999] font-[500] tracking-[0.0125rem] cursor-pointer ${
           selectedNeutered === "all" ? 'bg-mainColor text-white ' : 'bg-[#EFEFF0] text-[#999]'
         }`}
         onClick={() => handleSelect("all")}
