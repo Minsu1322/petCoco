41952d2e70a10746772c0d895d64b027dbca600d
hyeonw8 <whgusrud223@gmail.com>
2024-08-21 02:40:42 +0900

    fix: margin 간격 수정

--- src/app/(public)/mate/posts/_components/detailView.tsx
+++ src/app/(public)/mate/posts/_components/detailView.tsx
@@ -160,19 +160,19 @@ const DetailView = ({ post, userId, handleEditPost, handleDeletePost, handleTogg
               </p>
             </div>
             <div className="flex items-center">
-            <div className="h-[1.25rem] w-[1.25rem]">
-                      <Image
-                        src="/assets/svg/ic_user2.svg"
-                        alt="사용자 아이콘"
-                        width={20}
-                        height={20}
-                        priority
-                        className="h-full w-full object-cover"
-                      />
-                    </div>
-              <p className="mr-[0.5rem] flex text-[1rem] font-[400]">{post.members}명 모집</p>
+              <div className="h-[1.25rem] w-[1.25rem]">
+                <Image
+                  src="/assets/svg/ic_user2.svg"
+                  alt="사용자 아이콘"
+                  width={20}
+                  height={20}
+                  priority
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <p className="ml-[0.5rem] flex text-[1rem] font-[400]">{post.members}명 모집</p>
               <div
-                className={`${post.recruiting ? "bg-[#11BBB0]" : "bg-bgGray400"} flex items-center justify-center rounded-full px-[0.62rem] py-[0.12rem] text-white`}
+                className={`${post.recruiting ? "bg-[#11BBB0]" : "bg-bgGray400"} flex items-center justify-center rounded-full px-[0.62rem] py-[0.12rem] ml-[0.5rem] text-white`}
               >
                 <p className="text-[0.875rem] font-[400]">{post.recruiting ? "모집중" : "모집 완료"}</p>
               </div>
